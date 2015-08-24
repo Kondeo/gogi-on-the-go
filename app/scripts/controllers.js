4781277e6a35a742ea44a8efae014257d7ebb3e0
Julian Poyourow <julianpoyo@gmail.com>
2015-08-24 13:57:30 -0700

    Updated tool page with dynamic properties

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -147,7 +147,7 @@ angular.module('starter.controllers', [])
                 else
                 {
                     //go to our tool page
-                    $location.path("app/tool/" + currentTool);
+                    $location.path("app/tool/" + currentTool).search({featured: 'true'});
                 }
                 //And break
                 break;
@@ -214,8 +214,16 @@ angular.module('starter.controllers', [])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
-.controller('ToolCtrl', function($scope, $stateParams) {
+.controller('ToolCtrl', function($scope, $stateParams, $location, $ionicHistory) {
   $scope.tool = getByID($scope.contentItems, parseInt($stateParams.toolId));
+
+  $scope.pageTitleText = "Tool Details"
+  $scope.titleSubText = "a gogi tool.";
+  if($location.search().featured){
+      $ionicHistory.clearHistory();
+      $scope.pageTitleText = "Featured Tool"
+      $scope.titleSubText = "featured tool this month."
+  }
 
   function getByID(arr, id){
     for (var d = 0, len = arr.length; d < len; d += 1) {
